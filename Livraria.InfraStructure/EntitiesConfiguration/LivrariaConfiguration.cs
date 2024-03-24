f208a7f8ac41616e6a1a2ffc442c3e81117e069a
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2024-03-24 20:04:53 -0300

    Finalizado InfraStruct

--- Livraria.InfraStructure/EntitiesConfiguration/LivrariaConfiguration.cs
+++ Livraria.InfraStructure/EntitiesConfiguration/LivrariaConfiguration.cs
@@ -1,3 +1,7 @@
+using Livraria.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -5,8 +9,15 @@ using System.Threading.Tasks;
 
 namespace Livraria.InfraStructure.EntitiesConfiguration
 {
-    public class LivrariaConfiguration
+    public class LivrariaConfiguration : IEntityTypeConfiguration<Livro>
     {
-        
+        public void Configure(EntityTypeBuilder<Livro> builder)
+        {
+            builder.HasKey(t => t.LivroId);
+            builder.Property(p => p.Titulo).HasMaxLength(300).IsRequired();
+            builder.Property(p => p.Autor).HasMaxLength(300).IsRequired();
+            builder.Property(p => p.Lancamento).IsRequired();
+            builder.Property(p => p.Capa).HasMaxLength(200);
+        }
     }
 }
--- Livraria.InfraStructure/Repositories/LivroRepository.cs
+++ Livraria.InfraStructure/Repositories/LivroRepository.cs
@@ -1,3 +1,5 @@
+using Livraria.Domain.Abstractions;
+using Livraria.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -5,8 +7,36 @@ using System.Threading.Tasks;
 
 namespace Livraria.InfraStructure.Repositories
 {
-    public class LivroRepository
+    public class LivroRepository : ILivroRepository
     {
-        
+        public Task<Livro> AdcionarLivro(Livro livro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AtualizaLivro(Livro livro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeletarLivro(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Livro>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Livro?> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Livro?> GetByName(string name)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
