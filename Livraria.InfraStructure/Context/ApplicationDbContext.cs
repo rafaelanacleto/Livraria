abd726c83fb4294ad89f182ce0ff423b73ad5890
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2024-03-24 17:32:12 -0300

    Recompilação reinicio do gitignore para ver se sai os arquivos duplicados

--- Livraria.InfraStructure/Context/ApplicationDbContext.cs
+++ Livraria.InfraStructure/Context/ApplicationDbContext.cs
@@ -1,3 +1,5 @@
+using Livraria.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -5,8 +7,19 @@ using System.Threading.Tasks;
 
 namespace Livraria.InfraStructure.Context
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
-        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Livro>? Livros { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+
     }
 }
